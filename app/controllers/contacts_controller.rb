727cd636069b93d5af90eff0259424e33359eb5d
David Collins <davidcollins1991@gmail.com>
2017-07-05 10:46:36 +0000

    Add some form validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,26 @@
 class ContactsController < ApplicationController
-  #create new instance var
+  #executes when contact action is run
+  #create new instance variable
   def new
     @contact = Contact.new
   end
-  
+  #Use the create action to save something to a DB by default in rails
   def create
+    #saving details from form and apply to @contact
+    #mass assignment
     @contact = Contact.new(contact_params)
+    #.save saves to DB
+    #if save successful, redirect user back to blank page with message
     if @contact.save
       redirect_to new_contact_path, notice: "Message Sent."
+    #if it fails also redirect back but show fail message
     else 
       redirect_to new_contact_path, notice: "Error Occured."
     end
   end
   
   private
+  #take anything from form fields securely 
    def contact_params
      params.require(:contact).permit(:name, :email, :comments)
    end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-  
+  #validates from to be filled out
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
