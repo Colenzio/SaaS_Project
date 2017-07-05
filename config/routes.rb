ce225c043608b45c597296d288e8a71330bbcd42
David Collins <davidcollins1991@gmail.com>
2017-07-05 17:18:07 +0000

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   #shortcut for generating paths
   #provides various contact pages
-  resources :contacts
+  #limit user routes to :create
+  resources :contacts, only: :create
   #points custom url to contacts/new
-  get 'contact-us', to: 'contacts#new'
+  #contact_us will change to new_contact us path
+  get 'contact-us', to: 'contacts#new', as: "new_contact"
 end
