8c4ce122015e76934ef0dccccced6df9ae43caf7
David Collins <davidcollins1991@gmail.com>
2017-07-06 09:36:40 +0000

    Added contact view mailer form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,8 @@
+#file to send contact details to my email
+#inherits ActionMailer 
 class ContactMailer < ActionMailer::Base
   default to: "davidcolenzio@hotmail.com"
+
   
   def contact_email(name, email, body)
     @name = name
