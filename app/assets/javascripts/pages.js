e7db5838a4af1a74f140b94fdd05783dbafd5c7b
David Collins <davidcollins1991@gmail.com>
2017-07-12 16:44:40 +0000

    Updated flash message style and JS

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,4 @@
+$(document).on('turbolinks:load', function(){
+  // alert fade out
+  $('.alert').delay(1000).fadeOut(4000);
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,10 @@ class ApplicationController < ActionController::Base
   # If the user is submitting a form through the deivse controller, run action
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+   # Whitelist these form fields so can be processed
 protected
   def configure_permitted_parameters
-    # Whitelist these parameters
+    
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
   end
 end
