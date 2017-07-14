9511ac4424e83f3b5bcf7f214fd618496fd8957e
David Collins <davidcollins1991@gmail.com>
2017-07-14 11:13:12 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # when devise creates a user, we devise it know which type of user it is
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do 
+    resource :profile
+  end  
   # when about select, change to about page
   get 'about', to: 'pages#about'
   # shortcut for generating paths
