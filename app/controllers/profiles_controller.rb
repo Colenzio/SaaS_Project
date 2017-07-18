d523ae9b0358330f4183a61a4bec6a3125820483
David Collins <davidcollins1991@gmail.com>
2017-07-18 13:37:08 +0000

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,29 @@
 class ProfilesController < ApplicationController
   
-  # GET to users/:user:id/profile/new
+  # GET to users/:user_id/profile/new
   def new
     # Render blank profile details form
     @profile = Profile.new
   end
+  
+  # POST to users/:user_id/profile
+  def create
+    # Ensure we have the user that is filling out form
+    @user =  User.find( params[:user_id] )
+    # Build out a profile in memory using whitelisted details below
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile Updated"
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  
+  private 
+  
+  # Whitelist these params, only these things can be exepted into DB
+   def profile_params
+     params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+   end
 end
