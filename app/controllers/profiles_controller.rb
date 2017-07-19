b849c48964eb4696392348f12bee3582baea3318
David Collins <davidcollins1991@gmail.com>
2017-07-19 12:06:29 +0000

    Added avatar to profile table

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,6 +14,7 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params )
     if @profile.save
       flash[:success] = "Profile Updated"
+      # redirect user to their profile page
       redirect_to user_path( params[:user_id] )
     else
       render action: :new
